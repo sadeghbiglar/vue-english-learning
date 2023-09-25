045a921eec6e09086d603a711b252bc79ce1dda7
sadeghbiglar <sbmail555@gmail.com>
2023-09-25 23:11:19 +0330

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <h1>Paria</h1>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">صفحه اصلی</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">پروفایل</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">امتیازات</a>
+          </li>
+        </ul>
+        <span class="navbar-text">اپلیکیشن آنلاین آموزش زبان انگلیسی پریا</span>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+import './assets/bootstrap.rtl.min.css'
+import './assets/bootstrap.bundle.min.js'
+//import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous"'
+import {
+    createApp
+} from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+//const app = createApp(App)
+//app.use(bootstrap)
+//app.mount('#app')
