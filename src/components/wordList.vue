f097ac190b8e83256e391dde77af6e1dca9fd31d
sadeghbiglar <sbmail555@gmail.com>
2023-09-29 18:03:22 +0330

    reset words status when complete every stage and show new stage

--- src/components/wordList.vue
+++ src/components/wordList.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <h1>لیست کلمات</h1>
     <div class="row">
-      <div v-for="(item, index) in words" :key="index" class="col-lg-4 col-md-6 col-xl-3">
+      <div v-for="(item, index) in shuffledWords" :key="item.id" class="col-lg-4 col-md-6 col-xl-3">
         <div
           class="word-box"
           @click="toggleMeaning(index)"
@@ -32,7 +32,13 @@
         </div>
       </div>
     </div>
-    <div v-if="allWordsKnown" class="alert alert-success">موفق شدید!</div>
+
+    <div v-if="stage === 'stage2' && !showSuccessMessage2">
+      <div v-if="allWordsKnown" class="alert alert-success">موفق شدید!</div>
+    </div>
+    <div v-if="stage === 'stage3' && !showSuccessMessage3">
+      <div v-if="allWordsKnown" class="alert alert-success">عالی بود!</div>
+    </div>
   </div>
 </template>
 
@@ -41,7 +47,14 @@ export default {
   data() {
     return {
       words: [], // آرایه‌ای برای نگهداری کلمات
-      allWordsKnown: false // وضعیت بلد بودن همه کلمات
+      shuffledWords: [], // کلمات به ترتیب تصادفی
+      allWordsKnown: false, // وضعیت بلد بودن همه کلمات
+      stage: "stage1", // مرحله فعلی (stage1 یا stage2 یا stage3)
+      showSuccessMessage1: false, // نمایش پیام موفقیت مرحله 1
+      showSuccessMessage2: false, // نمایش پیام موفقیت مرحله 2
+      showSuccessMessage3: false, // نمایش پیام موفقیت مرحله 3
+      correctWords: [], // آرایه‌ای برای ذخیره کلمات درست
+      wrongWords: [] // آرایه‌ای برای ذخیره کلمات غلط
     };
   },
   mounted() {
@@ -56,27 +69,59 @@ export default {
           throw new Error("Unable to fetch data");
         }
         this.words = await response.json();
-        // اضافه کردن ویژگی showMeaning به هر کلمه برای نمایش معنی
-        this.words.forEach(word => (word.showMeaning = false));
+        // اضافه کردن ویژگی‌های showMeaning و known به هر کلمه برای نمایش معنی و بلد بودن
+        this.words.forEach(word => {
+          word.showMeaning = false;
+          word.known = false;
+        });
+        // ترتیب تصادفی کلمات
+        this.shuffledWords = this.shuffleArray(this.words.slice());
       } catch (error) {
         console.error("Error loading words:", error);
       }
     },
     toggleMeaning(index) {
       // تغییر وضعیت نمایش معنی با کلیک روی باکس
-      this.words[index].showMeaning = !this.words[index].showMeaning;
+      this.shuffledWords[index].showMeaning = !this.shuffledWords[index]
+        .showMeaning;
     },
     markAsKnown(index) {
       // علامت‌گذاری به عنوان "بلد بودم"
-      this.words[index].known = true;
+      this.shuffledWords[index].known = true;
       // بررسی آیا همه کلمات بلد هستند
-      this.allWordsKnown = this.words.every(word => word.known);
+      this.allWordsKnown = this.shuffledWords.every(word => word.known);
+      if (this.allWordsKnown && this.stage == "stage1") {
+        this.stage = "stage2";
+        this.showSuccessMessage1 = true;
+        this.resetWordStatus();
+      } else if (this.allWordsKnown && this.stage == "stage2") {
+        this.stage = "stage3";
+        this.showSuccessMessage1 = true;
+      }
     },
     markAsUnknown(index) {
       // علامت‌گذاری به عنوان "بلد نبودم"
-      this.words[index].known = false;
-      // کنترل مجدد برای پنهان کردن پیام هشدار
-      this.allWordsKnown = false;
+      this.shuffledWords[index].known = false;
+    },
+    shuffleArray(array) {
+      // تابعی برای تصادفی کردن یک آرایه
+      let shuffledArray = array.slice();
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [
+          shuffledArray[j],
+          shuffledArray[i]
+        ];
+      }
+      return shuffledArray;
+    },
+    resetWordStatus() {
+      // بازنشانی وضعیت کلمات برای شروع مرحله جدید
+      this.shuffledWords.forEach(word => {
+        word.showMeaning = false;
+        word.known = false;
+      });
+      this.shuffledWords = this.shuffleArray(this.words.slice());
     }
   }
 };
