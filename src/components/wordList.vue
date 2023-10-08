3e40f256612048ea2ef9162dceb4e16d2c78ae3c
sadeghbiglar <sbmail555@gmail.com>
2023-10-09 00:59:38 +0330

    add correct and wrong words into seprate array in stage3 and shuffle unkwon words in other stages

--- src/components/wordList.vue
+++ src/components/wordList.vue
@@ -33,13 +33,6 @@
         </div>
       </div>
     </div>
-
-    <div v-if="stage === 'stage2' && showSuccessMessage2">
-      <div v-if="allWordsKnown" class="alert alert-success">موفق شدید!</div>
-    </div>
-    <div v-if="stage === 'stage3' && showSuccessMessage3">
-      <div v-if="allWordsKnown" class="alert alert-success">عالی بود!</div>
-    </div>
   </div>
 </template>
 
@@ -50,21 +43,15 @@ const words = ref([]); // آرایه‌ای برای نگهداری کلمات
 const shuffledWords = ref([]); // کلمات به ترتیب تصادفی
 const allWordsKnown = ref(false); // وضعیت بلد بودن همه کلمات
 const stage = ref("stage1"); // مرحله فعلی (stage1 یا stage2 یا stage3)
-const showSuccessMessage1 = ref(false); // نمایش پیام موفقیت مرحله 1
-const showSuccessMessage2 = ref(false); // نمایش پیام موفقیت مرحله 2
-const showSuccessMessage3 = ref(false); // نمایش پیام موفقیت مرحله 3
 const correctWords = ref([]); // آرایه‌ای برای ذخیره کلمات درست
 const wrongWords = ref([]); // آرایه‌ای برای ذخیره کلمات غلط
-const show_meaning_click = ref(false);
-const currentProgress = ref(0);
-const maxProgress = ref(100);
 const stage1Progress = ref(0);
 const stage2Progress = ref(0);
 const stage3Progress = ref(0);
 const totalProgress = 100;
 const loadWords = async () => {
   try {
-    const response = await fetch("/src/assets/words.json"); // مسیر فایل JSON را تعیین کنید
+    const response = await fetch("/src/assets/words.json");
     if (!response.ok) {
       throw new Error("Unable to fetch data");
     }
@@ -90,28 +77,46 @@ const toggleMeaning = index => {
 const markAsKnown = index => {
   // علامت‌گذاری به عنوان "بلد بودم"
   shuffledWords.value[index].known = true;
-
   // حذف کلمه از آرایه shuffledWords
-  shuffledWords.value.splice(index, 1);
 
-  // بررسی آیا همه کلمات بلد هستند
+  if (stage.value === "stage3") {
+    const removedValue = shuffledWords.value.splice(index, 1)[0];
+    correctWords.value.push(removedValue);
+  } else {
+    shuffledWords.value.splice(index, 1);
+  }
+  // بررسی آیا همه کلمات بلدبودم هستند
   allWordsKnown.value = shuffledWords.value.length === 0;
   if (allWordsKnown.value && stage.value === "stage1") {
     stage.value = "stage2";
-    showSuccessMessage2.value = true;
     stage1Progress.value = 30; // مرحله 1
     resetWordStatus();
-    console.log(shuffledWords.value);
+    // console.log(shuffledWords.value);
   } else if (allWordsKnown.value && stage.value === "stage2") {
     stage.value = "stage3";
-    showSuccessMessage3.value = true;
     stage2Progress.value = 60; // مرحله 2
+    resetWordStatus();
+    // console.log(shuffledWords.value);
+  } else if (allWordsKnown.value && stage.value === "stage3") {
+    stage.value = "stage_end";
+    stage3Progress.value = 100; // مرحله 3
+    //resetWordStatus();
+    // console.log(shuffledWords.value);
+    console.log(correctWords.value);
+    console.log(wrongWords.value);
   }
 };
 
 const markAsUnknown = index => {
   // علامت‌گذاری به عنوان "بلد نبودم"
-  shuffledWords.value[index].known = false;
+
+  if (stage.value === "stage3") {
+    shuffledWords.value[index].known = false;
+    const removedValue = shuffledWords.value.splice(index, 1)[0];
+    wrongWords.value.push(removedValue);
+  } else {
+    shuffledWords.value = shuffleArray([...shuffledWords.value]);
+  }
 };
 
 const shuffleArray = array => {
